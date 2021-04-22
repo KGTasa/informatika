1641729e5138fdb10d7000de44c60243d31c4719
KGTasa <44865315+KGTasa@users.noreply.github.com>
2021-04-22 13:56:22 +0200

    Update tri_kruga.py

--- tri_kruga.py
+++ tri_kruga.py
@@ -1,18 +1,18 @@
 import pygame
 pygame.init()
 (sirina,duzina)=(400,400)
-prozor=pg.display.set_mode((sirina, duzina))
+prozor=pygame.display.set_mode((sirina, duzina))
 pygame.display.set_caption("Tri Kruga")
-prozor.fill(pg.Color('White'))
+prozor.fill(pygame.Color('White'))
 
 (cx,cy)=(sirina//2, duzina//2)
 poluprecnik1=125
 poluprecnik2=100
 poluprecnik3=75
 
-pg.draw.circle(prozor, pg.Color('black'), (cx, cy), poluprecnik1, 2)
-pg.draw.circle(prozor, pg.Color('yellow'), (cx, cy), poluprecnik2, 2)
-pg.draw.circle(prozor, pg.Color('green'), (cx, cy), poluprecnik3, 2)
+pygame.draw.circle(prozor, pygame.Color('black'), (cx, cy), poluprecnik1, 2)
+pygame.draw.circle(prozor, pygame.Color('yellow'), (cx, cy), poluprecnik2, 2)
+pygame.draw.circle(prozor, pygame.Color('green'), (cx, cy), poluprecnik3, 2)
 
 pygame.display.update()
 while pygame.event.wait().type !=pygame.QUIT:
